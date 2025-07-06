96ce43f3a8974387b1f2f8fe4165a6cf36b9c190
vitthal4545 <vitthalgs1434@gmail.com>
2025-07-06 17:27:00 +0530

    environmental setup

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+//routes
+
+app.get("/", (req, res, next) => {
+    res.json({ message: "welcome to elib apis" });
+});
+
 export default app;
